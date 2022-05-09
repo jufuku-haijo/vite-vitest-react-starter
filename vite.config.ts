abc6267b92cab3c993d353b892588fc0805e5ffd
cijiugechu <cijiugechu@gmail.com>
2022-05-10 00:58:49 +0800

    feat: resolve path alias: `@`

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgrPlugin from 'vite-plugin-svgr'
 import { book } from 'vite-plugin-book/vite'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,6 +18,12 @@ export default defineConfig({
     }),
     book()
   ],
+
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    }
+  },
 
   test: {
     globals: true,
